dc849cf0168ba62875caa96f13670c9b556b20c6
Ruben Montes <rubenmontes330@gmail.com>
2023-02-25 18:19:25 +0000

    header y llamada a la APi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import {React, useState} from 'react'
+import './header.css'
+import { Formik, Form, Field } from 'formik';
+const App = () => {
+  const [photos, setPhotos]= useState([])
+  const open = url => window.open(url)
+  console.log({photos});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{search:''}}
+          onSubmit={async values => {
+            // llamar a api de unsplash
+            const response= await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,{
+              headers: {
+                'Authorization': 'Client-ID C2ptUBx1_7OsDnO4-BhpYiOTFQ-OVwGIWF0nY7XVKFA'
+              }
+            })
+            const data= await response.json()
+            setPhotos(data.results)
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name='search'/>
+          </Form>
+
+        </Formik>
       </header>
+      <div className='container'>
+        <div className='center'>
+          {photos.map(photo => 
+            <article key={photo.id} onClick={()=> open(photo.links.html)}>
+              <img src={photo.urls.regular}/>
+              <p>{[photo.description, photo.alt_description].join( ' - ')}</p>
+            </article>)}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
